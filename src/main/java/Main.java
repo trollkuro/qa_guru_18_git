6006e92af19298ec7ea5728586803b62b3b000a8
trollkuro <kristi.egorova@gmail.com>
2023-09-08 18:52:27 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
+    private static String msg = ""Hello qa.guru!!!"";
+
     public static void main(String[] args){
-        System.out.println("Hello qa.guru");
+        System.out.println(msg);
     }
 }
