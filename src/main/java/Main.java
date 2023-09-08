6778bb04b05f78b1745b84dde7d45d6eaa7249c7
trollkuro <kristi.egorova@gmail.com>
2023-09-08 19:04:28 +0300

    Update Main.java to see a conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    private static String msg = ""Hello qa.guru!!!"";
+    private static String msg = "Hello qa.guru!";
 
     public static void main(String[] args){
         System.out.println(msg);
